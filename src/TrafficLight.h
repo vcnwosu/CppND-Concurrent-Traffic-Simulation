2dd8bd51a7e72cf200895406f53a0822e8b02e59
vcnwosu <vcnwosu@gmail.com>
2020-02-08 20:37:26 -0600

    Task FP.3: Define MessageQueue class

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -19,9 +19,12 @@ template <class T>
 class MessageQueue
 {
 public:
-
+    T receive();
+    T send(T &&msg);
 private:
-    
+    std::deque<T> _queue;
+    std::mutex _mtx;
+    std::condition_variable _cond;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
